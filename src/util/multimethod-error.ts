0a1988e9efe31cbc8415b92688fb29bafefe3872
yortus <yortus@gmail.com>
2016-10-05 17:13:11 +0800

    RoutistError --> MultimethodError

--- src/util/multimethod-error.ts
+++ src/util/multimethod-error.ts
@@ -4,7 +4,7 @@
 
 
 // TODO: doc...
-export default class RoutistError extends Error {
+export default class MultimethodError extends Error {
 
     constructor(message: string) {
         super(message);
