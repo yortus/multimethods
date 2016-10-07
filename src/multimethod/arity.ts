3898ab5dadf7c268ce51518823d7b139c62a3fe1
yortus <yortus@gmail.com>
2016-10-07 10:41:37 +0800

    MM class factory WIP III

--- src/multimethod/arity.ts
+++
@@ -1,7 +0,0 @@
-
-
-
-
-
-type Arity = 1 | 2 | 3 | 'variadic';
-export default Arity;
--- src/multimethod/multimethod.ts
+++ src/multimethod/multimethod.ts
@@ -1,97 +1,7 @@
-import Arity from './arity';
-import createMultimethodClass from './create-multimethod-class';
 import {MultimethodError} from '../util';
 
 
 
-
-
-// ================================================================================
-// TODO: Unary case...
-export interface UnaryMultimethodConstructor {
-    new<T0, TR>(options: UnaryMultimethodOptions<T0, TR | Promise<TR>> & {async?: 'mixed'}): UnaryMultimethod<T0, TR | Promise<TR>>;
-    new<T0, TR>(options: UnaryMultimethodOptions<T0, Promise<TR>> & {async?: true}): UnaryMultimethod<T0, Promise<TR>>;
-    new<T0, TR>(options: UnaryMultimethodOptions<T0, TR> & {async?: false}): UnaryMultimethod<T0, TR>;
-}
-export interface UnaryMultimethod<T0, TR> extends Multimethod {
-    ($0: T0): TR;
-    add(methods: {[predicate: string]: UnaryMethod<T0, TR>}): this;
-    add(predicate: string, consequent: UnaryMethod<T0, TR>): this;
-}
-export const UnaryMultimethod: UnaryMultimethodConstructor = class UnaryMultimethod extends createMultimethodClass(1) { };
-export interface UnaryMultimethodOptions<T0, TR> extends MultimethodOptions {
-    toDiscriminant?: ($0: T0) => string;
-    methods?: {[predicate: string]: UnaryMethod<T0, TR>};
-}
-export interface UnaryMethod<T0, TR> {
-    ($0: T0, captures: Captures, next: Next): TR;
-}
-
-
-// ================================================================================
-// TODO: Binary case...
-export interface BinaryMultimethodConstructor {
-    new<T0, T1, TR>(options: BinaryMultimethodOptions<T0, T1, TR | Promise<TR>> & {async?: 'mixed'}): BinaryMultimethod<T0, T1, TR | Promise<TR>>;
-    new<T0, T1, TR>(options: BinaryMultimethodOptions<T0, T1, Promise<TR>> & {async?: true}): BinaryMultimethod<T0, T1, Promise<TR>>;
-    new<T0, T1, TR>(options: BinaryMultimethodOptions<T0, T1, TR> & {async?: false}): BinaryMultimethod<T0, T1, TR>;
-}
-export interface BinaryMultimethod<T0, T1, TR> extends Multimethod {
-    ($0: T0, $1: T1): TR;
-    add(methods: {[predicate: string]: BinaryMethod<T0, T1, TR>}): this;
-    add(predicate: string, consequent: BinaryMethod<T0, T1, TR>): this;
-}
-export const BinaryMultimethod: BinaryMultimethodConstructor = createMultimethodClass(2);
-export interface BinaryMultimethodOptions<T0, T1, TR> extends MultimethodOptions {
-    toDiscriminant?: ($0: T0, $1: T1) => string;
-    methods?: {[predicate: string]: BinaryMethod<T0, T1, TR>};
-}
-export interface BinaryMethod<T0, T1, TR> {
-    ($0: T0, $1: T1, captures: Captures, next: Next): TR;
-}
-
-
-// ================================================================================
-// TODO: Ternary case...
-export interface TernaryMultimethodConstructor {
-    new<T0, T1, T2, TR>(options: TernaryMultimethodOptions<T0, T1, T2, TR | Promise<TR>> & {async?: 'mixed'}): TernaryMultimethod<T0, T1, T2, TR | Promise<TR>>;
-    new<T0, T1, T2, TR>(options: TernaryMultimethodOptions<T0, T1, T2, Promise<TR>> & {async?: true}): TernaryMultimethod<T0, T1, T2, Promise<TR>>;
-    new<T0, T1, T2, TR>(options: TernaryMultimethodOptions<T0, T1, T2, TR> & {async?: false}): TernaryMultimethod<T0, T1, T2, TR>;
-}
-export interface TernaryMultimethod<T0, T1, T2, TR> extends Multimethod {
-    ($0: T0, $1: T1, $2: T2): TR;
-    add(methods: {[predicate: string]: TernaryMethod<T0, T1, T2, TR>}): this;
-    add(predicate: string, consequent: TernaryMethod<T0, T1, T2, TR>): this;
-}
-export const TernaryMultimethod: TernaryMultimethodConstructor = createMultimethodClass(3);
-export interface TernaryMultimethodOptions<T0, T1, T2, TR> extends MultimethodOptions {
-    toDiscriminant?: ($0: T0, $1: T1, $2: T2) => string;
-    methods?: {[predicate: string]: TernaryMethod<T0, T1, T2, TR>};
-}
-export interface TernaryMethod<T0, T1, T2, TR> {
-    ($0: T0, $1: T1, $2: T2, captures: Captures, next: Next): TR;
-}
-
-
-// ================================================================================
-// TODO: Variadic case...
-export interface VariadicMultimethodConstructor {
-    new<T, TR>(options: VariadicMultimethodOptions<T, TR | Promise<TR>> & {async?: 'mixed'}): VariadicMultimethod<T, TR | Promise<TR>>;
-    new<T, TR>(options: VariadicMultimethodOptions<T, Promise<TR>> & {async?: true}): VariadicMultimethod<T, Promise<TR>>;
-    new<T, TR>(options: VariadicMultimethodOptions<T, TR> & {async?: false}): VariadicMultimethod<T, TR>;
-}
-export interface VariadicMultimethod<T, TR> extends Multimethod {
-    (...args: T[]): TR;
-    add(methods: {[predicate: string]: VariadicMethod<T, TR>}): this;
-    add(predicate: string, consequent: VariadicMethod<T, TR>): this;
-}
-export const VariadicMultimethod: VariadicMultimethodConstructor = createMultimethodClass('variadic');
-export interface VariadicMultimethodOptions<T, TR> extends MultimethodOptions {
-    toDiscriminant?: (args: T[]) => string;
-    methods?: {[predicate: string]: VariadicMethod<T, TR>};
-}
-export interface VariadicMethod<T, TR> {
-    (args: T[], captures: Captures, next: Next): TR;
-}
 
 
 // ================================================================================
@@ -112,7 +22,11 @@ export interface MultimethodConstructor {
     new(options: MultimethodOptions): Multimethod;
 }
 export const Multimethod: MultimethodConstructor = createMultimethodClass(undefined);
-export interface Multimethod extends Function { }
+export interface Multimethod {
+    (...args: any[]): any;
+    add(methods: {[predicate: string]: Function}): this;
+    add(predicate: string, consequent: Function): this;
+}
 export interface MultimethodOptions {
     arity?: Arity;
     async?: true | false | 'mixed';
@@ -126,9 +40,163 @@ export default Multimethod;
 
 
 // ================================================================================
+// TODO: Unary case...
+export interface UnaryMultimethodConstructor {
+    new<T0, TR>(options: UnaryMultimethodOptions<T0, TR | Promise<TR>> & {async?: 'mixed'}): UnaryMultimethod<T0, TR | Promise<TR>>;
+    new<T0, TR>(options: UnaryMultimethodOptions<T0, Promise<TR>> & {async?: true}): UnaryMultimethod<T0, Promise<TR>>;
+    new<T0, TR>(options: UnaryMultimethodOptions<T0, TR> & {async?: false}): UnaryMultimethod<T0, TR>;
+}
+export interface UnaryMultimethod<T0, TR> extends Multimethod {
+    ($0: T0): TR;
+    add(methods: {[predicate: string]: UnaryMethod<T0, TR>}): this;
+    add(predicate: string, consequent: UnaryMethod<T0, TR>): this;
+}
+export const UnaryMultimethod: UnaryMultimethodConstructor = class UnaryMultimethod extends createMultimethodClass(1) { };
+export interface UnaryMultimethodOptions<T0, TR> extends MultimethodOptions {
+    arity?: 1;
+    toDiscriminant?: ($0: T0) => string;
+    methods?: {[predicate: string]: UnaryMethod<T0, TR>};
+}
+export interface UnaryMethod<T0, TR> {
+    ($0: T0, captures: Captures, next: Next): TR;
+}
+
+
+// ================================================================================
+// TODO: Binary case...
+export interface BinaryMultimethodConstructor {
+    new<T0, T1, TR>(options: BinaryMultimethodOptions<T0, T1, TR | Promise<TR>> & {async?: 'mixed'}): BinaryMultimethod<T0, T1, TR | Promise<TR>>;
+    new<T0, T1, TR>(options: BinaryMultimethodOptions<T0, T1, Promise<TR>> & {async?: true}): BinaryMultimethod<T0, T1, Promise<TR>>;
+    new<T0, T1, TR>(options: BinaryMultimethodOptions<T0, T1, TR> & {async?: false}): BinaryMultimethod<T0, T1, TR>;
+}
+export interface BinaryMultimethod<T0, T1, TR> extends Multimethod {
+    ($0: T0, $1: T1): TR;
+    add(methods: {[predicate: string]: BinaryMethod<T0, T1, TR>}): this;
+    add(predicate: string, consequent: BinaryMethod<T0, T1, TR>): this;
+}
+export const BinaryMultimethod: BinaryMultimethodConstructor = class BinaryMultimethod extends createMultimethodClass(2) { };
+export interface BinaryMultimethodOptions<T0, T1, TR> extends MultimethodOptions {
+    arity?: 2;
+    toDiscriminant?: ($0: T0, $1: T1) => string;
+    methods?: {[predicate: string]: BinaryMethod<T0, T1, TR>};
+}
+export interface BinaryMethod<T0, T1, TR> {
+    ($0: T0, $1: T1, captures: Captures, next: Next): TR;
+}
+
+
+// ================================================================================
+// TODO: Ternary case...
+export interface TernaryMultimethodConstructor {
+    new<T0, T1, T2, TR>(options: TernaryMultimethodOptions<T0, T1, T2, TR | Promise<TR>> & {async?: 'mixed'}): TernaryMultimethod<T0, T1, T2, TR | Promise<TR>>;
+    new<T0, T1, T2, TR>(options: TernaryMultimethodOptions<T0, T1, T2, Promise<TR>> & {async?: true}): TernaryMultimethod<T0, T1, T2, Promise<TR>>;
+    new<T0, T1, T2, TR>(options: TernaryMultimethodOptions<T0, T1, T2, TR> & {async?: false}): TernaryMultimethod<T0, T1, T2, TR>;
+}
+export interface TernaryMultimethod<T0, T1, T2, TR> extends Multimethod {
+    ($0: T0, $1: T1, $2: T2): TR;
+    add(methods: {[predicate: string]: TernaryMethod<T0, T1, T2, TR>}): this;
+    add(predicate: string, consequent: TernaryMethod<T0, T1, T2, TR>): this;
+}
+export const TernaryMultimethod: TernaryMultimethodConstructor = class TernaryMultimethod extends createMultimethodClass(3) { };
+export interface TernaryMultimethodOptions<T0, T1, T2, TR> extends MultimethodOptions {
+    arity?: 3;
+    toDiscriminant?: ($0: T0, $1: T1, $2: T2) => string;
+    methods?: {[predicate: string]: TernaryMethod<T0, T1, T2, TR>};
+}
+export interface TernaryMethod<T0, T1, T2, TR> {
+    ($0: T0, $1: T1, $2: T2, captures: Captures, next: Next): TR;
+}
+
+
+// ================================================================================
+// TODO: Variadic case...
+export interface VariadicMultimethodConstructor {
+    new<T, TR>(options: VariadicMultimethodOptions<T, TR | Promise<TR>> & {async?: 'mixed'}): VariadicMultimethod<T, TR | Promise<TR>>;
+    new<T, TR>(options: VariadicMultimethodOptions<T, Promise<TR>> & {async?: true}): VariadicMultimethod<T, Promise<TR>>;
+    new<T, TR>(options: VariadicMultimethodOptions<T, TR> & {async?: false}): VariadicMultimethod<T, TR>;
+}
+export interface VariadicMultimethod<T, TR> extends Multimethod {
+    (...args: T[]): TR;
+    add(methods: {[predicate: string]: VariadicMethod<T, TR>}): this;
+    add(predicate: string, consequent: VariadicMethod<T, TR>): this;
+}
+export const VariadicMultimethod: VariadicMultimethodConstructor = class VariadicMultimethod extends createMultimethodClass('variadic') { };
+export interface VariadicMultimethodOptions<T, TR> extends MultimethodOptions {
+    arity?: 'variadic';
+    toDiscriminant?: (args: T[]) => string;
+    methods?: {[predicate: string]: VariadicMethod<T, TR>};
+}
+export interface VariadicMethod<T, TR> {
+    (args: T[], captures: Captures, next: Next): TR;
+}
+
+
+
+
+
+// ================================================================================
 // Multimethod class defined as decomposed static-side and instance-side interfaces.
 // NB: defined this way to achieve type-parameterization in the constructor (class decls don;t allow this, see TS #10860).
 // TODO: default arity === 1 - how to express safely in type system?
+
+
+
+
+
+// TODO: ...
+export type Arity = 1 | 2 | 3 | 'variadic';
+
+
+
+
+
+// TODO: ...
+function createMultimethodClass(staticArity?: Arity): MultimethodConstructor {
+
+    return <any> class Multimethod {
+        constructor(options: {arity?: Arity}) {
+
+            // If *both* arities given, they must match
+            if (staticArity && options.arity && staticArity !== options.arity) {
+                throw new MultimethodError(`arity mismatch`); // TODO: improve diagnostic message
+            }
+
+            // Use whichever arity is given. If neither arity is given, default to 'variadic'.
+            let arity = staticArity || options.arity || 'variadic';
+
+            // TODO: ...
+            let instance = createMultimethod(arity, options);
+            instance[CTOR] = Multimethod;
+            return instance;
+        }
+
+        static [Symbol.hasInstance](value: any) {
+            if (staticArity) {
+                return value && value[CTOR] === Multimethod;
+            }
+            else {
+                return value && value.hasOwnProperty(CTOR); // TODO: works for symbols?
+            }
+        }
+    }
+}
+
+
+
+
+
+// TODO: ...
+function createMultimethod(arity: Arity, options: {}) {
+    // TODO: ...
+    return <any> {};
+}
+
+
+
+
+
+// TODO: ...
+const CTOR = Symbol('ctor');
 
 
 
