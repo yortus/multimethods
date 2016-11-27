ae9546d7f8da71bd5c01b712d0cfb5c9ef3f7141
yortus <yortus@gmail.com>
2016-11-27 12:55:14 +0800

    move route executor template into own file

--- src/multimethod/codegen/create-route-executor.ts
+++ src/multimethod/codegen/create-route-executor.ts
@@ -2,6 +2,7 @@
 import * as util from '../../util';
 import MultimethodOptions from '../multimethod-options';
 import RouteExecutor from './route-executor';
+import routeExecutorTemplate from './route-executor-template';
 import Rule from '../impl/rule';
 
 
@@ -106,7 +107,7 @@ function generateRouteExecutorSourceCode(rules: Rule[], ruleNames: string[], opt
 
 
         // TODO:
-        let source = template.toString();
+        let source = routeExecutorTemplate.toString();
         source = normaliseSource(source);
         source = eliminateDeadCode(source, {
             STARTS_PARTITION: i === 0 || method.isMetaRule,
@@ -140,90 +141,6 @@ function generateRouteExecutorSourceCode(rules: Rule[], ruleNames: string[], opt
 
     // Combine and return all the sources.
     return sources.join('\n');
-}
-
-
-
-
-
-// TODO: explain each of these in turn...
-let UNHANDLED: any;
-let STARTS_PARTITION: boolean;
-let ENDS_PARTITION: boolean;
-let HAS_CAPTURES: boolean;
-let IS_META_RULE: boolean;
-let IS_PURE_SYNC: boolean;
-let IS_PURE_ASYNC: boolean;
-let METHOD_NAME: string;
-let DELEGATE_DOWNSTREAM: RouteExecutor;
-let DELEGATE_NEXT: RouteExecutor;
-let GET_CAPTURES: (discriminant: string) => {};
-let CALL_METHOD: (context: any, ...args: any[]) => any; // Method signature
-let FROZEN_EMPTY_OBJECT: {};
-
-
-// TODO: note ES6 in source here - spread and rest (...MM_ARGS)
-// TODO: explain important norms in the template function...
-// TODO: don't need to dedent any more!
-// TODO: put more explanatory comments inside, and strip them out to maximise inlining potential
-let template = function METHOD_NAME(discriminant: string, result: any, ...MM_ARGS: any[]) {
-
-    // TODO: explain why result is passed in and checked here (hint: unified code for sync/async handling)
-    if (!STARTS_PARTITION) {
-        if (result !== UNHANDLED) {
-            return result;
-        }
-    }
-
-    // TODO: set up context...
-    if (HAS_CAPTURES) {
-        var context = GET_CAPTURES(discriminant);
-    }
-    else {
-        if (IS_META_RULE) {
-            var context = {};
-        }
-        else {
-            var context = FROZEN_EMPTY_OBJECT;
-        }
-    }
-
-    // TODO: meta rules...
-    if (IS_META_RULE) {
-        context['next'] = function (...MM_ARGS) {
-            return DELEGATE_DOWNSTREAM(discriminant, UNHANDLED, ...MM_ARGS);
-        };
-    }
-
-    // TODO: call method...
-    result = CALL_METHOD(context, ...MM_ARGS);
-
-    // TODO: cascade result...
-    if (!ENDS_PARTITION) {
-        if (IS_PURE_SYNC) {
-
-            // All methods in this MM are synchronous
-            result = DELEGATE_NEXT(discriminant, result, ...MM_ARGS);
-        }
-        else {
-            if (IS_PURE_ASYNC) {
-
-                // All methods in this MM are asynchronous
-                result = result.then(rs => DELEGATE_NEXT(discriminant, rs, ...MM_ARGS));
-            }
-            else {
-
-                // Methods may be sync or async, and we must differentiate at runtime
-                if (isPromise(result)) {
-                    result = result.then(rs => DELEGATE_NEXT(discriminant, rs, ...MM_ARGS));
-                }
-                else {
-                    result = DELEGATE_NEXT(discriminant, result, ...MM_ARGS);
-                }
-            }
-        }
-    }
-    return result;
 }
 
 
---
+++ src/multimethod/codegen/route-executor-template.ts
@@ -0,0 +1,91 @@
+import RouteExecutor from './route-executor';
+
+
+
+
+
+// TODO: how to ensure this is defined? Just doc?
+let isPromise: (x: any) => boolean;
+
+
+
+
+
+// TODO: explain each of these in turn...
+let UNHANDLED: any;
+let STARTS_PARTITION: boolean;
+let ENDS_PARTITION: boolean;
+let HAS_CAPTURES: boolean;
+let IS_META_RULE: boolean;
+let IS_PURE_SYNC: boolean;
+let IS_PURE_ASYNC: boolean;
+let DELEGATE_DOWNSTREAM: RouteExecutor;
+let DELEGATE_NEXT: RouteExecutor;
+let GET_CAPTURES: (discriminant: string) => {};
+let CALL_METHOD: (context: any, ...args: any[]) => any; // Method signature
+let FROZEN_EMPTY_OBJECT: {};
+
+
+// TODO: note ES6 in source here - spread and rest (...MM_ARGS)
+// TODO: explain important norms in the template function...
+// TODO: don't need to dedent any more!
+// TODO: put more explanatory comments inside, and strip them out to maximise inlining potential
+export default function METHOD_NAME(discriminant: string, result: any, ...MM_ARGS: any[]) {
+
+    // TODO: explain why result is passed in and checked here (hint: unified code for sync/async handling)
+    if (!STARTS_PARTITION) {
+        if (result !== UNHANDLED) {
+            return result;
+        }
+    }
+
+    // TODO: set up context...
+    if (HAS_CAPTURES) {
+        var context = GET_CAPTURES(discriminant);
+    }
+    else {
+        if (IS_META_RULE) {
+            var context = {};
+        }
+        else {
+            var context = FROZEN_EMPTY_OBJECT;
+        }
+    }
+
+    // TODO: meta rules...
+    if (IS_META_RULE) {
+        context['next'] = function (...MM_ARGS) {
+            return DELEGATE_DOWNSTREAM(discriminant, UNHANDLED, ...MM_ARGS);
+        };
+    }
+
+    // TODO: call method...
+    result = CALL_METHOD(context, ...MM_ARGS);
+
+    // TODO: cascade result...
+    if (!ENDS_PARTITION) {
+        if (IS_PURE_SYNC) {
+
+            // All methods in this MM are synchronous
+            result = DELEGATE_NEXT(discriminant, result, ...MM_ARGS);
+        }
+        else {
+            if (IS_PURE_ASYNC) {
+
+                // All methods in this MM are asynchronous
+                result = result.then(rs => DELEGATE_NEXT(discriminant, rs, ...MM_ARGS));
+            }
+            else {
+
+                // Methods may be sync or async, and we must differentiate at runtime
+                if (isPromise(result)) {
+                    result = result.then(rs => DELEGATE_NEXT(discriminant, rs, ...MM_ARGS));
+                }
+                else {
+                    result = DELEGATE_NEXT(discriminant, result, ...MM_ARGS);
+                }
+            }
+        }
+    }
+    return result;
+}
