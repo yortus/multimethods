8d0b96a0009853f9122916a292c690e380f9f901
yortus <yortus@gmail.com>
2016-11-30 11:02:44 +0800

    remove some dead code

--- src/multimethod/codegen/create-route-executors.ts
+++ src/multimethod/codegen/create-route-executors.ts
@@ -21,28 +21,20 @@ export type WithRoute = {route: Rule[]}
 // TODO: ...
 export default function createRouteExecutors(taxonomy: Taxonomy<WithRoute>, normalisedOptions: MultimethodOptions) {
 
-    // TODO: temp testing...
+    // Bring things into local scope that are ref'd from eval'ed code. NB: the source code
+    // for eval cannot safely refer directly to expressions like `util.isPromiseLike`, since the `util` identifier may not
+    // appear in the transpiled JavaScript for this module. This is because TypeScript may rename modules to try to preserve
+    // ES6 module semantics.
+    const isPromise = util.isPromiseLike;
     const UNHANDLED = normalisedOptions.unhandled;
-
-    // TODO: temp testing...
-//let routes = new Map(taxonomy.allNodes.map(node => [node.pattern, node.route] as [Pattern, Rule[]]));
-    //let allRules: Rule[] = [...new Set([].concat(...routes.values())).values()];
+    const ℙØ = (discriminant, result) => result; // ND: *always* called with result = UNHANDLED
 
     // Generate the combined source code for handling the route. This includes local variable declarations for
     // all rules' matchers and methods, as well as the interdependent function declarations that perform
     // the cascading, and possibly asynchronous, evaluation of the route.
     let lines = [
-
-        // ...allRules
-        //     .map((rule, i) => `var ${getCapturesFor}${getNameForRule(rule)} = allRules[${i}].predicate.match;`)
-        //     .filter((_, i) => allRules[i].predicate.captureNames.length > 0),
-
-        // ...allRules.map((rule, i) => `var ${invokeMethodFor}${getNameForRule(rule)} = allRules[${i}].method;`),
-
         `return {`,
-
         ...taxonomy.allNodes.map(node => generateExecutorSourceCode(node, normalisedOptions)),
-
         `};`
     ];
 
@@ -64,24 +56,6 @@ export default function createRouteExecutors(taxonomy: Taxonomy<WithRoute>, norm
 
 
 
-// Declare isPromise in module scope, so the eval'ed route handlers below can reference it. NB: the source code
-// for eval cannot safely refer directly to expressions like `util.isPromiseLike`, since the `util` identifier may not
-// appear in the transpiled JavaScript for this module. This is because TypeScript may rename modules to try to preserve
-// ES6 module semantics.
-const isPromise = util.isPromiseLike;
-
-
-
-
-
-// TODO: temp testing... these are ref'ed in codegen code and never change
-const FROZEN_EMPTY_OBJECT = Object.freeze({});
-const ℙØ = (discriminant, result) => result; // ND: *always* called with result = UNHANDLED
-
-
-
-
-
 // TODO: ...
 const getCapturesFor = 'getCapturesFor';
 const invokeMethodFor = 'invokeMethodFor';
@@ -90,8 +64,8 @@ const invokeMethodFor = 'invokeMethodFor';
 
 
 
-// TODO: was... superseded by above/below code... extract out comments/useful stuff then remove...
-// TODO: rewrite comment. Esp signature of route executor matches signature of multimethod (as per provided Options)
+// TODO: rewrite comments. Esp signature of route executor matches signature of multimethod (as per provided Options)
+
 /**
  * Generates the composite method for the route described by the given `rules`.
  * In the absence of meta-rules, the logic for executing a route is straightforward: execute the method for each rule in
@@ -105,53 +79,12 @@ const invokeMethodFor = 'invokeMethodFor';
  * @param {Rule[]} rules - the list of rules comprising the route, ordered from least- to most-specific.
  * @returns {Method} the composite method for the route.
  */
-// function createRouteExecutor(pattern: Pattern, rules: Rule[], options: MultimethodOptions): RouteExecutor {
-//     // TODO: pattern parameter not used - remove it or use it?
-
-//     // TODO: temp testing...
-//     const UNHANDLED = options.unhandled;
-
-//     // Obtain a reversed copy of the rule list, ordered from most- to least-specific. This simplifies logic below.
-//     rules = rules.slice().reverse();
-
-//     // The 'start' rule is the one whose method we call to begin the cascading evaluation of the route. It is the
-//     // least-specific meta-rule, or if there are no meta-rules, it is the most-specific ordinary rule.
-//     let startMethodName = (rules.filter(rule => rule.isMetaRule).pop() || rules[0]).name;
-
-//     // Generate the combined source code for handling the route. This includes local variable declarations for
-//     // all rules' matchers and methods, as well as the interdependent function declarations that perform
-//     // the cascading, and possibly asynchronous, evaluation of the route.
-//     let lines = [
-//         ...rules
-//             .map((rule, i) => `var ${getCapturesFor}${rule.name} = rules[${i}].predicate.match;`)
-//             .filter((_, i) => rules[i].predicate.captureNames.length > 0),
-//         ...rules.map((rule, i) => `var ${invokeMethodFor}${rule.name} = rules[${i}].method;`),
-//         generateExecutorSourceCode(rules, options),
-//         `return ${startMethodName};`
-//     ];
-
-//     // FOR DEBUGGING: uncomment the following line to see the generated code for each route executor at runtime.
-//     console.log(`\n\n\n================ ROUTE EXECUTOR for ${startMethodName} ================\n${lines.join('\n')}`);
-
-// // TODO: switch to `new Function` with closed over vars passed as params (as done in bluebird)
-//     // Evaluate the source code, and return its result, which is the composite route handler function. The use of eval
-//     // here is safe. There are no untrusted inputs substituted into the source. The client-provided rule handler
-//     // functions can do anything (so may be considered untrusted), but that has nothing to do with the use of 'eval'
-//     // here, since they would need to be called by the route handler whether or not eval was used. More importantly,
-//     // the use of eval here allows for route handler code that is both more readable and more efficient, since it is
-//     // tailored specifically to the route being evaluated, rather than having to be generalized for all possible cases.
-//     let fn = eval(`(() => {\n${lines.join('\n')}\n})`)();
-//     return fn;
-// }
-
-
-
-
 
 /**
  * Helper function to generate source code for a set of interdependent functions (one per rule) that perform the
  * cascading evaluation of a route, accounting for the possibly mixed sync/async implementation of the rule handlers.
  */
+
 function generateExecutorSourceCode(node: TaxonomyNode & WithRoute, options: MultimethodOptions): string {
 
     // Obtain a reversed copy of the rule list, ordered from most- to least-specific. This simplifies logic below.
@@ -173,7 +106,6 @@ function generateExecutorSourceCode(node: TaxonomyNode & WithRoute, options: Mul
         //   of this file). It is substituted in as the value of `$next` when a meta-rule's method is called.
         // - `handlerArgs` is a hash keyed by all possible parameter names a rule's raw handler may use, and whose
         //   values are the source code for the argument corresponding to each parameter.
-
 
         // TODO:
         let source = routeExecutorTemplate.toString();
@@ -215,6 +147,7 @@ if (rule.predicate.captureNames.length > 0) {
     source = `var ${getCapturesFor}${rule.predicate.identifier}_${i} = taxonomy.get('${node.pattern.normalized}').route[${i}].predicate.match;\n${source}`
 }
 
+        // All done for this iteration.
         return source;
     });
 
@@ -226,27 +159,6 @@ if (rule.predicate.captureNames.length > 0) {
 let namesake = rules[0].predicate.identifier;
     return `\n    ${namesake}: (function () {\n${sources.join('\n')}\n        return ${entryPointName};\n    })(),`;  // TODO: ES2017 trailing comma will break stuff!
 }
-
-
-
-
-
-// TODO: temp testing...
-// function getNameForRule(rule: Rule) {
-//     return rule.name;
-
-//     // TODO: audit below - ensure it is not possible to craft a pattern whose identifier will be the same as an augmented rule name generated by this function
-
-//     // if (rule.method.name === '_unhandled') {
-//     //     return 'BIGDADDY'; // TODO: temp fix... always last method tried on every route (UNIVERSAL ROOT)
-//     // }
-//     // if (rule.method.name === '_ambiguous') {
-//     //     return rule.predicate.identifier + '_ambiguous'; // TODO: see above comment - this *could* clash with another pattern identifier that was crafted to do that
-//     // }
-//     // else {
-//     //     return rule.predicate.identifier;
-//     // }
-// }
 
 
 
