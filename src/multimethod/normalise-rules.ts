1d90f906196da08872060fb6de006e26b25985b5
yortus <yortus@gmail.com>
2017-06-30 16:16:48 +0800

    outlaw giving rules with the same normal predicate

--- src/multimethod/normalise-rules.ts
+++ src/multimethod/normalise-rules.ts
@@ -1,9 +1,11 @@
 import {CONTINUE} from './sentinels';
 import debug, {DISPATCH} from '../util/debug';
+import fatalError from '../util/fatal-error';
 import isPromiseLike from '../util/is-promise-like';
 import isMetaHandler from './is-meta-handler';
 import MultimethodOptions from './multimethod-options';
 import Rule from './rule';
+import {toPredicate, toNormalPredicate} from '../set-theory/predicates';
 
 
 
@@ -17,6 +19,23 @@ import Rule from './rule';
 
 // TODO: ...
 export default function normaliseRules(rules: MultimethodOptions['rules']) {
+
+    // TODO: temp tesing...
+    // - ensure all rules have valid predicates
+    // - ensure no two rules have the same normalised predicate
+    let deduped = {} as {[s: string]: string[]};
+    Object.keys(rules).forEach(predicateSource => {
+        let p = toPredicate(predicateSource);
+        let np = toNormalPredicate(p);
+        deduped[np] = deduped[np] || [];
+        deduped[np].push(p);
+    });
+    for (let np in deduped) {
+        if (deduped[np].length <= 1) continue;
+        fatalError('DUPLICATE_PREDICATE', np, `'${deduped[np].join(`', '`)}'`);
+    }
+
+    // TODO: doc...
     let result: Rule[] = [];
     for (let predicate in rules) {
         let handler = rules[predicate];
--- src/util/fatal-error.ts
+++ src/util/fatal-error.ts
@@ -8,6 +8,7 @@ export default fatalError;
 
 function fatalError(error: 'AMBIGUOUS_RULE_ORDER', ruleA: string, ruleB: string): never;
 function fatalError(error: 'ARITY_MISMATCH'): never;
+function fatalError(error: 'DUPLICATE_PREDICATE', normalised: string, predicates: string): never;
 function fatalError(error: 'MULTIPLE_FALLBACKS_FROM', predicate: string, fallbacks: string): never;
 function fatalError(error: 'MULTIPLE_PATHS_TO', predicate: string): never;
 function fatalError(error: 'MIXED_CHAIN', predicate: string): never;
@@ -27,6 +28,7 @@ function fatalError(error: keyof typeof messages, ...params: (string|number)[]):
 export const messages = {
     'AMBIGUOUS_RULE_ORDER': `Ambiguous rule ordering - which is more specific of '%s' and '%s'?`,
     'ARITY_MISMATCH': `arity mismatch`, // TODO: improve diagnostic message
+    'DUPLICATE_PREDICATE': `The predicate '%s' is duplicated across multiple rules: %s. To resolve this, use a handler chain.`,
     'MIXED_CHAIN': `Chain for predicate '%s' has metahandler(s) to the right of regular handler(s). Metahandlers must be leftmost in the chain.`,
     'MULTIPLE_FALLBACKS_FROM': `Multiple possible fallbacks from '%s': %s`,
     'MULTIPLE_PATHS_TO': `Multiple paths to '%s' with different meta-rules`,
