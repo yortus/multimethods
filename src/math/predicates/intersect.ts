99bc5367997796fc91ffaceaaf0eed6098c0236b
yortus <yortus@gmail.com>
2017-08-09 13:49:50 +0800

    intersect(): refactor with early returns to reduce nesting

--- src/math/predicates/intersect.ts
+++ src/math/predicates/intersect.ts
@@ -92,40 +92,45 @@ function getAllIntersections(a: NormalPredicate, b: NormalPredicate): NormalPred
     // so sorting `a` and `b` reduces the solution space without affecting the result.
     if (a > b) [b, a] = [a, b];
 
-    // If an intersection was already computed for these `a` and `b` values, return the previous result.
+    // If an intersection was already computed for these `a` and `b` values, return the previously memoised result.
+    // All code paths beyond this point memoise the result before returning it, so it becomes available to future calls.
     let memoisedResult = MEMOISER.get(a, b);
     if (memoisedResult) return memoisedResult;
     ++CALL_COUNT;
-
-    // TODO: doc...
-    let result = [] as NormalPredicate[];
 
     // CASE 1: Either predicate is empty. An empty predicate intersects only with
     // another empty predicate or a single wildcard/globstar. Since an empty string
     // precedes all other strings lexicographically, we just check if `a` is empty.
     if (a === '') {
         if (b === '' || b === '*' || b === '**') {
-            result.push('' as NormalPredicate);
+            return MEMOISER.set(a, b, ['' as NormalPredicate]);
+        }
+        else {
+            return MEMOISER.set(a, b, []);
         }
     }
 
     // CASE 2: Either predicate is found to be a subset of the other, using
     // simple pattern matching. The result in this case is the subset predicate.
-    else if (isSubsetOf(a, b)) {
-        result.push(a);
+    if (isSubsetOf(a, b)) {
+        return MEMOISER.set(a, b, [a]);
     }
-    else if (isSubsetOf(b, a)) {
-        result.push(b);
+    if (isSubsetOf(b, a)) {
+        return MEMOISER.set(a, b, [b]);
     }
 
     // CASE 3: Both predicates start with a literal character. If the starting
     // literals differ, the predicates are disjoint. Otherwise, we determine
     // the intersection by intersecting their remainders recursively.
-    else if (isLiteral(a.charAt(0)) && isLiteral(b.charAt(0))) {
+    if (isLiteral(a.charAt(0)) && isLiteral(b.charAt(0))) {
         if (a.charAt(0) === b.charAt(0)) {
             let aAfterFirst = a.slice(1) as NormalPredicate;
             let bAfterFirst = b.slice(1) as NormalPredicate;
-            result = getAllIntersections(aAfterFirst, bAfterFirst).map(u => a.charAt(0) + u) as NormalPredicate[];
+            let result = getAllIntersections(aAfterFirst, bAfterFirst).map(u => a.charAt(0) + u) as NormalPredicate[];
+            return MEMOISER.set(a, b, result);
+        }
+        else {
+            return MEMOISER.set(a, b, []);
         }
     }
 
@@ -133,35 +138,31 @@ function getAllIntersections(a: NormalPredicate, b: NormalPredicate): NormalPred
     // way in which the second predicate may be split into a [prefix, suffix] pair. For each such pair where the
     // prefix unifies with the starting wildcard/globstar of the first predicate, we recursively intersect the
     // suffix with the remainder of the first predicate. All successful unifications are accumulated into the result.
-    else {
-        let [first, second] = startsWithGlobstar(a) || isLiteral(b.charAt(0)) ? [a, b] : [b, a];
+    let [first, second] = startsWithGlobstar(a) || isLiteral(b.charAt(0)) ? [a, b] : [b, a];
 
-        let firstChar = startsWithGlobstar(first) ? '**' : first.charAt(0);
-        let afterFirst = first.slice(firstChar.length) as NormalPredicate;
+    let firstChar = startsWithGlobstar(first) ? '**' : first.charAt(0);
+    let afterFirst = first.slice(firstChar.length) as NormalPredicate;
 
-        // Obtain all splits. When unifying splits against '*', do strength
-        // reduction on split prefixes containing '**' (ie replace '**' with '*').
-        let prefixSuffixPairs = getAllPredicateSplits(second);
-        if (isWildcard(firstChar)) {
-            prefixSuffixPairs.forEach(pair => pair[0] = pair[0].replace(/\*\*/g, '*') as NormalPredicate);
-        }
+    // Obtain all splits. When unifying splits against '*', do strength
+    // reduction on split prefixes containing '**' (ie replace '**' with '*').
+    let prefixSuffixPairs = getAllPredicateSplits(second);
+    if (isWildcard(firstChar)) {
+        prefixSuffixPairs.forEach(pair => pair[0] = pair[0].replace(/\*\*/g, '*') as NormalPredicate);
+    }
 
-        // Compute and return intersections for all valid unifications. This is a recursive operation.
-        for (let [prefix, suffix] of prefixSuffixPairs) {
-            let isUnifiable = firstChar === '**' || (prefix.indexOf('/') === -1 && prefix.indexOf('**') === -1);
-            if (!isUnifiable) continue;
+    // Compute and return intersections for all valid unifications. This is a recursive operation.
+    let result1 = [] as NormalPredicate[];
+    for (let [prefix, suffix] of prefixSuffixPairs) {
+        let isUnifiable = firstChar === '**' || (prefix.indexOf('/') === -1 && prefix.indexOf('**') === -1);
+        if (!isUnifiable) continue;
 
-            let more = getAllIntersections(afterFirst, suffix).map(u => prefix + u) as NormalPredicate[];
-            result = result.concat(more);
-        }
+        let more = getAllIntersections(afterFirst, suffix).map(u => prefix + u) as NormalPredicate[];
+        result1 = result1.concat(more);
     }
 
     // TODO: temp testing... dedupe
-    let result2 = result.length === 0 ? [] : toNormalPredicate(result.join('|')).split('|') as NormalPredicate[];
-
-    // Memoise the result before returning it, so that we can avoid computing it again in future.
-    MEMOISER.set(a, b, result2);
-    return result2;
+    let result2 = result1.length === 0 ? [] : toNormalPredicate(result1.join('|')).split('|') as NormalPredicate[];
+    return MEMOISER.set(a, b, result2);
 }
 
 
@@ -192,6 +193,7 @@ class Memoiser {
         let map2 = this.map.get(a);
         if (!map2) this.map.set(a, map2 = new Map());
         map2.set(b, value);
+        return value;
     }
     private map = new Map<NormalPredicate, Map<NormalPredicate, NormalPredicate[]>>();
 }
