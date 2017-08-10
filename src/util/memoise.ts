e99e602b4dbdd80e7caab7dea9bf24b164d8a2d3
yortus <yortus@gmail.com>
2017-08-10 15:29:39 +0800

    generalise memoiser

--- src/util/memoise.ts
+++ src/util/memoise.ts
@@ -4,18 +4,20 @@
 
 
 // TODO: doc or remove these restrictions:
-// - only works for binary functions
+// - only works for unary/binary/ternary functions, detected using fn.length
 // - only works for functions whose parameters are all primitive types (i.e. string, number, ...)
-// - only work for functions that *never* return undefined
 function memoise<T0 extends primitive, T1 extends primitive, TR>(fn: ($0: T0, $1: T1) => TR): ($0: T0, $1: T1) => TR;
+function memoise<T0 extends primitive, T1 extends primitive, T2 extends primitive, TR>(fn: ($0: T0, $1: T1, $2: T2) => TR): ($0: T0, $1: T1, $2: T2) => TR;
 function memoise(fn: Function) {
 
-    // This internal util only supports binary functions at the moment. Sanity check the input accordingly.
+    // This internal util only supports functions with arities of 1-3 at the moment. Sanity check the input accordingly.
     // If the sanity check fails that's an internal error, so we simply throw rather that using `fatalError`.
-    if (fn.length !== 2) throw new Error('Internal error: memoise only supports binary functions');
-
-    // Delegate to arity-specific implementation.
-    return makeBinaryMemoiser(fn as any);
+    switch (fn.length) {
+        case 1: return makeUnaryMemoiser(fn as any);
+        case 2: return makeBinaryMemoiser(fn as any);
+        case 3: return makeTernaryMemoiser(fn as any);
+        default: throw new Error(`Internal error: memoise: unsupported arity ${fn.length}`);
+    }
 }
 export default memoise;
 
@@ -31,20 +33,63 @@ export type primitive = string|number;
 
 
 // TODO: doc...
+function makeUnaryMemoiser<T0 extends primitive, TR>(fn: ($0: T0) => TR) {
+    const map0 = new Map<T0, TR>();
+    return ($0: T0): TR => {
+
+        // Return the previously memoised result, if any.
+        let value: TR|undefined;
+        if (map0.has($0)) return map0.get($0)!;
+
+        // Compute, memoise, and return the result.
+        value = fn($0);
+        map0.set($0, value);
+        return value;
+    };
+}
+
+
+
+
+
+// TODO: doc...
 function makeBinaryMemoiser<T0 extends primitive, T1 extends primitive, TR>(fn: ($0: T0, $1: T1) => TR) {
-    const map = new Map<T0, Map<T1, TR>>();
+    const map0 = new Map<T0, Map<T1, TR>>();
     return ($0: T0, $1: T1): TR => {
 
         // Return the previously memoised result, if any.
         let value: TR|undefined;
-        let map2 = map.get($0);
-        if (map2) value = map2.get($1);
-        if (value !== undefined) return value;
+        let map1 = map0.get($0);
+        if (!map1) map0.set($0, map1 = new Map());
+        if (map1.has($1)) return map1.get($1)!;
 
         // Compute, memoise, and return the result.
         value = fn($0, $1);
-        if (!map2) map.set($0, map2 = new Map());
-        map2.set($1, value);
+        map1.set($1, value);
+        return value;
+    };
+}
+
+
+
+
+
+// TODO: doc...
+function makeTernaryMemoiser<T0 extends primitive, T1 extends primitive, T2 extends primitive, TR>(fn: ($0: T0, $1: T1, $2: T2) => TR) {
+    const map0 = new Map<T0, Map<T1, Map<T2, TR>>>();
+    return ($0: T0, $1: T1, $2: T2): TR => {
+
+        // Return the previously memoised result, if any.
+        let value: TR|undefined;
+        let map1 = map0.get($0);
+        if (!map1) map0.set($0, map1 = new Map());
+        let map2 = map1.get($1);
+        if (!map2) map1.set($1, map2 = new Map());
+        if (map2.has($2)) return map2.get($2)!;
+
+        // Compute, memoise, and return the result.
+        value = fn($0, $1, $2);
+        map2.set($2, value);
         return value;
     };
 }
